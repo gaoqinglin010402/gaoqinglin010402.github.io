2e341e0ea6a61fc5c80cc49f14fe3d32b1373426
gaoqinglin010402 <2834176905@qq.com>
2019-11-25 13:29:55 +0800

    another commit

---
+++ shiyan/js/new_file.js
@@ -0,0 +1,17 @@
+//function sayMagicWord(){
+//	var paragraph=document.getElementById("result");
+//	paragraph.innerHTML="PLEASE!";
+//}
+
+
+
+
+
+//alert("Is?");
+//function compute(){
+//	var input1=document.getElementById("num1");
+//	var input2=document.getElementById("num2");
+//	var answer=document.getElementById("answer");
+//	var result=input1.value*input2.value;
+//	answer.innerHTML=result;
+//}
